c1d0a70aac93d988b72c83bf2ea64259d81d75d5
Jakob Wonisch <jakob.wonisch@gmail.com>
2026-01-22 09:55:07 +0100

    add constraint generator

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { DATA_SIMPLE_GRAPH } from './data/simple.ts';
 import './style.css';
+import { ConstraintGenerator } from './util/ConstraintGenerator.ts';
 import { GraphHelper } from './util/GraphHelper.ts';
 import { parseGml } from './util/GraphParser.ts';
 
@@ -8,3 +9,4 @@ const graph = parseGml(DATA_SIMPLE_GRAPH);
 console.log(graph);
 
 const graphHelper = new GraphHelper(graph);
+const constraintGenerator = new ConstraintGenerator(graph);
